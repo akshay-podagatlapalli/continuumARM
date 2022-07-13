916037e3ad7d28e07301a8a04366ce40e833e891
Akshay <akshay.podagatlapalli@gmail.com>
2022-07-13 19:47:06 -0400

    added wrist coordinates

--- arm_data.py
+++ arm_data.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import numpy as np
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -26,12 +27,15 @@ with mp_hands.Hands(
     # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
-            print(
-            f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x}, '
-            f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y})' 
-            )
+            coordX = np.round(hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].x, 3)
+            coordY = np.round(hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y, 3)
+            coordZ = np.round(hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].z, 3)
+            
+            print(f'[{coordX}, {coordY}, {coordZ}]')
+
             mp_drawing.draw_landmarks(
                 image,
                 hand_landmarks,
